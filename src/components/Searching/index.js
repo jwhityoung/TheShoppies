d63d366f89c6da62f977daf0c6ac28c869143ad0
jwhityoung <jwhityoung@gmail.com>
2020-09-07 14:29:10 -0500

    heroku debugging

--- src/components/Searching/index.js
+++ src/components/Searching/index.js
@@ -3,15 +3,15 @@ import React from 'react';
 
 function  Searching (props){
     return (
-        null
-        // <li className="loading">
-        //     <div className="loading__poster"></div>
-        //     <div className="loading__details">
-        //         <div className="loading__title"></div>
-        //         <div className="loading__year"></div>
-        //     </div>
-        //     <div className="loading__button"></div>
-        // </li>
+      
+        <li className="loading">
+            <div className="loading__poster"></div>
+            <div className="loading__details">
+                <div className="loading__title"></div>
+                <div className="loading__year"></div>
+            </div>
+            <div className="loading__button"></div>
+        </li>
     )
     
 }
